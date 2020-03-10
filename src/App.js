0f8354d28384c2d84be011433f3b9bec8a2ab37d
Yari Andres <yari.andres@gmail.com>
2020-03-10 11:51:59 +0100

    adding components architecture/folders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      records: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://tools.hiwaldo.com/hiring/?key=wkbHE5F94aJRUJmE924DW4KBayrxWQ3q6LY8dtKf9PV')
+      .then(response => response.json())
+      .then(users => this.setState({ records: users.data }) )
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <CardList name='Yari'/>
+        {this.state.records.map(record => (<h1 key={record.id}>{record.name}</h1>))}
+      </div>
+    )
+  }
 }
+
+
 
 export default App;

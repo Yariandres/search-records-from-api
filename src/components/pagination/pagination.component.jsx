95203fc289d8a389946158da469fd748b310fa9d
Yari Andres <yari.andres@gmail.com>
2020-03-16 13:45:36 +0100

    url pagination fixture for netlify

--- src/components/pagination/pagination.component.jsx
+++ src/components/pagination/pagination.component.jsx
@@ -7,19 +7,19 @@ export const Pagination = props => {
 
   for(let i = 1; i >= props.pages; i++) {
     let active = props.currentPage === i ? 'active' : '';
-    pageLinks.push(<li className={`page ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="!#">{i}</a></li>)
+    pageLinks.push(<li className={`page ${active}`} key={i} onClick={() => props.nextPage(i)}><a href="#">{i}</a></li>)
   }
 
   return (
    
       <ul className="pagination">
         {props.currentPage > 1 ? <li className={`page`} onClick={() => props.nextPage(props.currentPage - 1)}>
-                                    <a className="prev" href="!#">Prev Page</a>
+                                    <a className="prev" href="#">Prev Page</a>
                                   </li> : ''}
         { pageLinks }
 
         {props.currentPage >= props.pages ? '' : <li className={`page`} onClick={() => props.nextPage(props.currentPage + 1)}>
-                                                    <a className="next" href="!#">Next Page</a>
+                                                    <a className="next" href="#">Next Page</a>
                                                 </li>}        
       </ul>
   );
